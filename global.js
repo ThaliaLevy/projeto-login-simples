e5e8a5805e554fefe949c64641cd0fc490048482
Thalia Levy <69092295+ThaliaLevy@users.noreply.github.com>
2023-02-24 21:01:44 -0300

    atualizando o projeto

--- global.js
+++ global.js
@@ -11,92 +11,93 @@ class Pessoa {
     }
 }
 
-function loginAdmin(){
-    pessoas.push(new Pessoa(
-        "admin",
-        "123")
-    );
-    
-}
+$(document).ready(() => {
+    if (sessionStorage.getItem("pessoas") == null) {
+        $("#email").val("admin");
+        $("#password").val(123);
+    }
+});
 
-function cadastrar() {
+$("#cadastrar").click(() => {
     pessoas.push(new Pessoa(
-        document.getElementById("email-admin").value,
-        document.getElementById("password-admin").value)
+        $("#email-admin").val(),
+        $("#password-admin").val())
     );
-    sessionStorage.setItem("pessoas", pessoas)
-    document.getElementById("email-admin").value = "";
-    document.getElementById("password-admin").value = "";
-    console.log(pessoas);
-}
+    sessionStorage.setItem("pessoas", JSON.stringify(pessoas));
+});
 
 function deixarInputVisivel() {
     document.getElementById("bloco1").className = "form-group";
     document.getElementById("bloco2").className = "form-group";
-    document.getElementById("email-editar").value = "";
-    document.getElementById("password-editar").value = "";
+    $("#email-editar").val("");
+    $("#password-editar").val("");
 }
 
-function editar() {
+$("#editar").click(() => {
     let aux = document.getElementById("email-admin").value;
     for (let i = 0; i < pessoas.length; i++) {
         if (pessoas[i] != "") {
             if (pessoas[i].email == aux) {
                 deixarInputVisivel();
                 indicePessoa = i;
-                document.getElementById("email-admin").value = "";
-                document.getElementById("password-admin").value = "";
+                $("#email-admin").val("");
+                $("#password-admin").val("");
                 break;
             }
         }
     }
     console.log(pessoas);
-}
+});
 
-function salvarEdicao() {
-    pessoas[indicePessoa].email = document.getElementById("email-editar").value;
-    pessoas[indicePessoa].senha = document.getElementById("password-editar").value;
+$("#salvarEdicao").click(() => {
+    pessoas[indicePessoa].email = $("#email-editar").val();
+    pessoas[indicePessoa].senha = $("#password-editar").val();
     console.log(pessoas);
     document.getElementById("bloco1").className = "form-group none";
     document.getElementById("bloco2").className = "form-group none";
-}
+});
 
-function excluir() {
-    let aux = document.getElementById("email-admin").value;
+$("#excluir").click(() => {
+    let aux = $("#email-admin").val();
     for (let i = 0; i < pessoas.length; i++) {
         if (pessoas[i] != "") {
             if (pessoas[i].email == aux) {
                 pessoas[i] = "";
-                document.getElementById("email-admin").value = "";
+                $("#email-admin").val("");
                 alert("Cadastro excluído!");
                 break;
             }
         }
     }
-    console.log(pessoas);
-}
+});
 
-function guardarPerfil() {
-    let aux = document.getElementById("opcPerfil").value;
+$("#guardarPerfil").click(() => {
+    let aux = $("#opcPerfil").val();
     sessionStorage.setItem("perfil", aux);
-}
+});
 
-function validarConta() {   //validar através do sessionStorage
-    let aux = sessionStorage.getItem("pessoas");
-    //verificar se é necessario converter para JSON
-    //sessionStorage.setItem("pessoas", JSON.stringify(pessoas));
-    
-    for (let i = 0; i < pessoas.length; i++) {
-        if (document.getElementById("email").value == pessoas[i].email) {
-            if (document.getElementById("password").value == pessoas[i].senha) {
+function validarConta() {
+    let aux = JSON.parse(sessionStorage.getItem("pessoas"));
+
+    if (sessionStorage.getItem("pessoas") != null) {
+        for (let i = 0; i < aux.length; i++) {
+            if (document.getElementById("email").value == aux[i].email && document.getElementById("password").value == aux[i].senha) {
                 return true;
-            }else{
+            } else {
+                document.getElementById("mensagem-erro").innerHTML = "E-mail ou senha incorretos!"
+            }
+        }
+    } else {
+        if (document.getElementById("email").value == "admin") {
+            if (document.getElementById("password").value == "123") {
+                return true;
+            } else {
                 document.getElementById("mensagem-erro").innerHTML = "Senha incorreta!"
             }
-        }else{
+        } else {
             document.getElementById("mensagem-erro").innerHTML = "E-mail incorreto!"
         }
-    }    
+    }
 }
 
 function redirecionar() {
@@ -105,8 +106,8 @@ function redirecionar() {
             window.location.href = "homeAdmin.html";
         }
     } else {
-        window.location.href = "homeUser.html";
+        if (validarConta() == true) {
+            window.location.href = "homeUser.html";
+        }
     }
-
-
 }
